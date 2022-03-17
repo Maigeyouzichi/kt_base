ab15321fceb5051242afd7657732dba2e71a42ea
lihao <243149100@qq.com>
2022-03-17 19:22:36 +0800

    内置函数+1

--- src/main/kotlin/v3_/ApplyTest.kt
+++ src/main/kotlin/v3_/ApplyTest.kt
@@ -1,23 +1,77 @@
 package v3_
 
+import sun.security.util.Password
+
 /**
- * apply -> 1, 返回值为其对象本身 2,this表示对象本身
+ * apply -> 1, 返回值为其对象本身 2,this表示对象本身 3,也支持具名函数方法引用
  * let ->   1,let的返回值根据最后一行确定(也符合匿名函数规则) 2,let种的it表示集合或者对象本身.
  * run ->   1, 返回值最后一行确定 2,持有this 3,支持匿名函数 4,支持具名函数
+ * with -> 类似run
+ * also -> 类似apply 1,返回obj本身 2, 使用it
+ * takeIf -> 大部分配合 :? 空操作符一起使用
  * @author lihao on 2022/3/17
  */
 fun main() {
-    ApplyTest().function03()
+    ApplyTest().function07()
 }
 
 class ApplyTest {
 
     /**
+     * takeIf
+     */
+    fun function07() {
+        var name = "lisi"
+        var password = "12345"
+        //如果checkPassword()返回TRUE,res=name,否则,res=null
+        val res = name.takeIf{checkPassword(name,password)}
+        res?.let { println("res: $res") } ?: println("密码错误")
+    }
+
+    fun checkPassword(name: String, password: String) : Boolean {
+        return name == "lisi" && password == "123456"
+    }
+
+    /**
+     * also
+     */
+    fun function06() {
+        var str = "周杰伦"
+        str.also(::printHello)
+            .also(::printWorld)
+    }
+
+    /**
+     * with
+     */
+    fun function05() {
+        var str = "周杰伦"
+        with(str,::printHello)
+        with(str,::printWorld)
+    }
+
+    /**
+     * apply
+     */
+    fun function04() {
+        var str: String = "Jay"
+        str.apply(::addHello).apply(::addWorld)
+    }
+    fun addHello(string: String) {
+        println("$string hello")
+    }
+
+    fun addWorld(string: String) {
+        println("$string world")
+    }
+    /**
      * run
      */
     fun function03() {
         var str = "Java"
-        str.run(::printHello).run(::printWorld)
+        str
+            .run(::printHello)
+            .run(::printWorld)//支持优雅的链式调用
         //str.run { printHello(this) }
     }
 
@@ -63,5 +117,6 @@ class ApplyTest {
         }
         println("info: $info")//hello world
         println("res: $res") //hello world  始终返回这个info本身
+
     }
 }
