2eb35642f6e4d3f969707d3f46428b4109b8ba48
lihao <243149100@qq.com>
2022-03-18 11:26:31 +0800

    kotlin -> class -> init{}

--- src/main/kotlin/v5_class/Class05.kt
+++ src/main/kotlin/v5_class/Class05.kt
@@ -6,17 +6,34 @@ package v5_class
  */
 //主构造
 class Class05() {
-    var name : String? = null
-    var sex : Char? = null
+    var name: String? = null
+    var sex: Char? = null
 
-    //次构造 -> 需要调动主构造
-    constructor(_name: String, _sex: Char) :this() {
+    //次构造 -> 需要先调动主构造
+    constructor(_name: String = "Java", _sex: Char) : this() {
         name = _name
         sex = _sex
     }
 }
 
+class Class06 {
+
+    // init块 -> 类似Java的构造块,构造方法执行前执行
+    init {
+        println("Hello World")
+    }
+
+    var name: String? = null
+    var age: Int? = null
+}
+
 fun main() {
-    val obj = Class05()
-    val obj2 = Class05("Java",'M')
+//    val obj = Class05()
+//    val obj2 = Class05(_sex = 'M')
+//    println(obj2.name)
+    val obj = Class06()
+    obj.name = "Java"
+    obj.age = 19
+    println(obj.name)
+    println(obj.age)
 }
