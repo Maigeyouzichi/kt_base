83e7839c5d43ea06162899b250a23f3156c44e59
lihao <243149100@qq.com>
2022-03-20 01:33:06 +0800

    kotlin -> class -> abstract class

--- src/main/kotlin/v6_open/Class19.kt
+++ src/main/kotlin/v6_open/Class19.kt
@@ -4,5 +4,27 @@ package v6_open
  * 抽象类
  * @author lihao on 2022/3/20
  */
-class Class19 {
+abstract class Class19 {
+    var age: Int = 19
+    fun function01(): Unit = run { println("Hello function01") }
+
+    abstract fun function02(): String
+    abstract fun function03(): Int
+}
+
+class Son01 : Class19() {
+    override fun function02(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun function03(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+
+fun main() {
+    val obj = Son01()
+    println(obj.age)
+    obj.function01()
 }
