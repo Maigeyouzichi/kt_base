4ff15395345dbb31baf4332b8167c3b0a7e1e447
lihao <243149100@qq.com>
2022-05-01 02:18:48 +0800

    测试代码 loop@

--- src/main/kotlin/v7/Test001.kt
+++ src/main/kotlin/v7/Test001.kt
@@ -7,7 +7,17 @@ package v7
 data class Test001(val id: Long, val name: String)
 
 fun main() {
-    var list = listOf<Test001>()
+    /*var list = listOf<Test001>()
     var map = list.associateBy { it.id }
-    println(map)
+    println(map)*/
+
+    loop@for (i in 1..5) {
+        println("i=$i")
+        for (j in 1..5) {
+            println("j=$j")
+            if (j ==2) break@loop
+        }
+        println("over inner")
+    }
+    println("over all")
 }
